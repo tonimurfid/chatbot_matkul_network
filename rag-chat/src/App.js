0bc65684a9e4b6197880c74257068b296555eb2d
tonii <fatonimurfids@gmail.com>
2025-04-22 19:48:34 +0700

    add debug 2

--- rag-chat/src/App.js
+++ rag-chat/src/App.js
@@ -37,7 +37,6 @@ function App() {
     setIsBotTyping(true);
 
     try {
-      console.log(process.env.REACT_APP_API_URL)
       const response = await fetch(`${process.env.REACT_APP_API_URL}`, {
         method: "POST",
         headers: {
